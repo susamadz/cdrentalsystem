03e9525c8aa2e4ceade1810a43ad01a5cc2156dd
Suzette <suzette0712@yahoo.com>
2013-12-27 23:55:49 +0800

    [new7] seventh db commit

--- db_script/movie_rent.sql
+++ db_script/movie_rent.sql
@@ -1,4 +1,4 @@
-﻿create tcustomer(cust_id char(9) not null,
+﻿create table tcustomer(cust_id char(9) not null,
 date_of_membership date, 
 cust_fname char(15) not null,
 cust_mname char(15),
@@ -7,9 +7,9 @@ cust_address char(25),
 cust_phone text,
 cust_email char (12),
 primary key(cust_id),
-unique(fname,lname));
+unique(cust_fname,cust_lname));
 
-create tcd(cd_id char(9) not null,
+create table tcd(cd_id char(9) not null,
 date_of_membership date, 
 cd_title char(10) not null,
 cd_genre char(10),
@@ -17,7 +17,9 @@ cd_year_release int,
 cd_rent_rate money not null,
 primary key(cd_id));
 
-create tcd_rent(rent_id char(9) not null,
+create table tcd_rent(rent_id char(9) not null,
+cust_id char(9),
+cd_id char(9),
 date_rented date not null,
 date_due date not null,
 date_return date,
@@ -25,24 +27,27 @@ primary key(rent_id),
 foreign key (cust_id)references tcustomer (cust_id),
 foreign key (cd_id)references tcd (cd_id));
 
-create payment(transact_id char(9) not null,
+create table payment(transact_id char(9) not null,
+rent_id char(9),
 transact_date date not null,
 transact_amount money not null,
 primary key(transact_id),
 foreign key (rent_id)references tcd_rent (rent_id));
 
 
-create cd_cast(cd_id char(9),
-actor_id char (9),
-primary key(cd_id, actor_id),
-foreign key (cd_id)references tcd (cd_id),
-foreign key (actor_id)references actor (actor_id));
-
-
-create actor (actor_id char(9) not null,
+create table actor (actor_id char(9) not null,
+rent_id char(9),
 actor_fname char(15) not null,
 actor_lname char(15) not null,
 actor_gender char (10),
 primary key (actor_id),
 foreign key (rent_id)references tcd_rent (rent_id),
 unique(actor_fname,actor_lname));
+
+create table cd_cast(cd_id char(9),
+actor_id char(9),
+primary key(cd_id, actor_id),
+foreign key (cd_id)references tcd (cd_id),
+foreign key (actor_id)references actor (actor_id));
+
+
