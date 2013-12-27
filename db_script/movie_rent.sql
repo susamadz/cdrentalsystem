6b0296b545a513a75c152dd9eda5ba10c081c0cf
Suzette <suzette0712@yahoo.com>
2013-12-27 22:21:32 +0800

    [new]

---
+++ db_script/movie_rent.sql
@@ -0,0 +1,10 @@
+﻿create tcustomer(cust_id char(9),
+date_of_membership date, 
+cust_fname char(15) not null,
+cust_mname char(15),
+cust_lname char(15) not null,
+cust_address char(25),
+cust_phone text,
+cust_email char (12),
+primary key(cust_id),
+unique(fname,lname));
