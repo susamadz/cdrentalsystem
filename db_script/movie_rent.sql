24797474d33b20698570be21d48461eceb089efb
Suzette <suzette0712@yahoo.com>
2013-12-28 21:01:13 +0800

    [new12] recovered file commit

--- db_script/movie_rent.sql
+++ db_script/movie_rent.sql
@@ -1,53 +1,92 @@
-﻿create table tcustomer(cust_id char(9) not null,
-date_of_membership date, 
-cust_fname char(15) not null,
-cust_mname char(15),
-cust_lname char(15) not null,
-cust_address char(25),
-cust_phone text,
-cust_email char (12),
-primary key(cust_id),
-unique(cust_fname,cust_lname));
+﻿INSERT INTO tcustomer (cust_id, date_of_membership, cust_fname, cust_mname, cust_lname, cust_address, cust_phone, cust_email) VALUES ('2000-0001', '2000-10-12', 'Sharon         ', 'Jarantilla     ', 'Falceso        ', 'Capitol, Pagadian City   ', '062-9250467', 'shynx_swear@yahoo.com    ');
+INSERT INTO tcustomer (cust_id, date_of_membership, cust_fname, cust_mname, cust_lname, cust_address, cust_phone, cust_email) VALUES ('2000-0002', '2000-11-22', 'Haide          ', 'Lamatan        ', 'Molejon        ', 'Capitol, Pagadian City   ', '062-9250234', 'hydzlam@yahoo.com        ');
+INSERT INTO tcustomer (cust_id, date_of_membership, cust_fname, cust_mname, cust_lname, cust_address, cust_phone, cust_email) VALUES ('2001-0003', '2001-02-12', 'Maynard        ', 'Balboa         ', 'Pilongo        ', 'Francisco. Pagadian City ', '062-9250281', 'adonis@yahoo.com         ');
+INSERT INTO tcustomer (cust_id, date_of_membership, cust_fname, cust_mname, cust_lname, cust_address, cust_phone, cust_email) VALUES ('2001-0004', '2001-05-23', 'Edilmer        ', 'Santos         ', 'Balbutin       ', 'Sunrise, Pagadian City   ', '062-9250786', 'remilde@yahoo.com        ');
+INSERT INTO tcustomer (cust_id, date_of_membership, cust_fname, cust_mname, cust_lname, cust_address, cust_phone, cust_email) VALUES ('2001-0005', '2001-09-16', 'Orven          ', 'Ebarle         ', 'Llantos        ', 'Iligan City              ', '088-389189', 'orven@yahoo.com          ');
 
-create table tcd(cd_id char(9) not null,
-date_of_membership date, 
-cd_title char(10) not null,
-cd_genre char(10),
-cd_year_release int,
-cd_rent_rate money not null,
-primary key(cd_id));
+INSERT INTO tcd (cd_id, cd_genre, cd_year_release, cd_rent_rate, cd_title) VALUES ('2013-1001', 'Fantasy   ', 2010, '$50.00', 'Batman                   ');
+INSERT INTO tcd (cd_id, cd_genre, cd_year_release, cd_rent_rate, cd_title) VALUES ('2013-1002', 'Romance   ', 2013, '$50.00', 'Twilight Saga            ');
+INSERT INTO tcd (cd_id, cd_genre, cd_year_release, cd_rent_rate, cd_title) VALUES ('2013-1003', 'Action    ', 2013, '$75.00', 'Homefront                ');
+INSERT INTO tcd (cd_id, cd_genre, cd_year_release, cd_rent_rate, cd_title) VALUES ('2013-1004', 'Action    ', 2013, '$75.00', 'Pain and Gain            ');
+INSERT INTO tcd (cd_id, cd_genre, cd_year_release, cd_rent_rate, cd_title) VALUES ('2013-1005', 'Action    ', 2013, '$75.00', 'Hunger Games             ');
 
-create table tcd_rent(rent_id char(9) not null,
-cust_id char(9),
-cd_id char(9),
-date_rented date not null,
-date_due date not null,
-date_return date,
-primary key(rent_id),
-foreign key (cust_id)references tcustomer (cust_id),
-foreign key (cd_id)references tcd (cd_id));
+INSERT INTO tcd_rent (rent_id, cust_id, cd_id, date_rented, date_due, date_return) VALUES ('000000001', '2001-0003', '2013-1003', '2013-12-22', '2013-12-23', '2013-12-23');
+INSERT INTO tcd_rent (rent_id, cust_id, cd_id, date_rented, date_due, date_return) VALUES ('000000002', '2000-0001', '2013-1002', '2013-12-22', '2013-12-23', '2013-12-23');
+INSERT INTO tcd_rent (rent_id, cust_id, cd_id, date_rented, date_due, date_return) VALUES ('000000003', '2001-0004', '2013-1001', '2013-12-25', '2013-12-26', '2013-12-27');
+INSERT INTO tcd_rent (rent_id, cust_id, cd_id, date_rented, date_due, date_return) VALUES ('000000004', '2000-0002', '2013-1005', '2013-12-26', '2013-12-27', '2013-12-27');
+INSERT INTO tcd_rent (rent_id, cust_id, cd_id, date_rented, date_due, date_return) VALUES ('000000005', '2001-0005', '2013-1004', '2013-12-26', '2013-12-27', '2013-12-27');
 
-create table payment(transact_id char(9) not null,
-rent_id char(9),
-transact_date date not null,
-transact_amount money not null,
-primary key(transact_id),
-foreign key (rent_id)references tcd_rent (rent_id));
+INSERT INTO actor (actor_id, actor_fname, actor_lname, actor_gender) VALUES ('5000     ', 'Jason          ', 'Statham        ', 'male      ');
+INSERT INTO actor (actor_id, actor_fname, actor_lname, actor_gender) VALUES ('5001     ', 'Jennifer       ', 'Lawrence       ', 'female    ');
+INSERT INTO actor (actor_id, actor_fname, actor_lname, actor_gender) VALUES ('5002     ', 'Mark           ', 'Wahlberg       ', 'male      ');
+INSERT INTO actor (actor_id, actor_fname, actor_lname, actor_gender) VALUES ('5003     ', 'Robert         ', 'De niro        ', 'male      ');
+INSERT INTO actor (actor_id, actor_fname, actor_lname, actor_gender) VALUES ('5004     ', 'Kristine       ', 'Stewart        ', 'female    ');
+
+INSERT INTO cd_cast (cd_id, actor_id) VALUES ('2013-1001', '5003     ');
+INSERT INTO cd_cast (cd_id, actor_id) VALUES ('2013-1002', '5004     ');
+INSERT INTO cd_cast (cd_id, actor_id) VALUES ('2013-1003', '5000     ');
+INSERT INTO cd_cast (cd_id, actor_id) VALUES ('2013-1004', '5002     ');
 
 
-create table actor (actor_id char(9) not null,
-rent_id char(9),
-actor_fname char(15) not null,
-actor_lname char(15) not null,
-actor_gender char (10),
-primary key (actor_id),
-foreign key (rent_id)references tcd_rent (rent_id),
-unique(actor_fname,actor_lname));
+--
+-- PostgreSQL database dump
+--
 
-create table cd_cast(cd_id char(9),
-actor_id char(9),
-primary key(cd_id, actor_id),
-foreign key (cd_id)references tcd (cd_id),
-foreign key (actor_id)references actor (actor_id));
+-- Dumped from database version 9.2.4
+-- Dumped by pg_dump version 9.2.4
+-- Started on 2013-12-28 14:13:57
 
+
+
+--
+-- TOC entry 1960 (class 0 OID 16527)
+-- Dependencies: 172
+-- Data for Name: actor; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+
+--
+-- TOC entry 1961 (class 0 OID 16539)
+-- Dependencies: 173
+-- Data for Name: cd_cast; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+
+
+--
+-- TOC entry 1959 (class 0 OID 16517)
+-- Dependencies: 171
+-- Data for Name: payment; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+
+
+--
+-- TOC entry 1957 (class 0 OID 16497)
+-- Dependencies: 169
+-- Data for Name: tcd; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+
+
+--
+-- TOC entry 1958 (class 0 OID 16502)
+-- Dependencies: 170
+-- Data for Name: tcd_rent; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+
+
+--
+-- TOC entry 1956 (class 0 OID 16487)
+-- Dependencies: 168
+-- Data for Name: tcustomer; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+
+-- Completed on 2013-12-28 14:14:00
+
+--
+-- PostgreSQL database dump complete
+--
 
